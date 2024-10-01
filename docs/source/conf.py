5d43d22fd03c7219969ad2579e8b005df5b1473c
imackay <innes.mackay@cern.ch>
2024-10-01 17:32:26 +0100

    Reset the conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,19 +14,18 @@ release = 'v1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',	     # To generate autodocs
-    'sphinx.ext.mathjax',           # autodoc with maths
-    'sphinx.ext.napoleon'           # For auto-doc configuration
-]
+#extensions = [
+#    'sphinx.ext.autodoc',	     # To generate autodocs
+#    'sphinx.ext.mathjax',           # autodoc with maths
+#    'sphinx.ext.napoleon'           # For auto-doc configuration
+#]
 
-napoleon_google_docstring = False   # Turn off googledoc strings
-napoleon_numpy_docstring = True     # Turn on numpydoc strings
-napoleon_use_ivar = True 	     # For maths symbology
+#napoleon_google_docstring = False   # Turn off googledoc strings
+#napoleon_numpy_docstring = True     # Turn on numpydoc strings
+#napoleon_use_ivar = True 	     # For maths symbology
 
 templates_path = ['_templates']
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------

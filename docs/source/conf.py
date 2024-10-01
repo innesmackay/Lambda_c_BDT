d92219de98f154d650c9b88a7cf56d10bf2c5ec1
imackay <innes.mackay@cern.ch>
2024-10-01 16:56:22 +0100

    Update read the docs configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,15 @@ release = 'v1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',	     # To generate autodocs
+    'sphinx.ext.mathjax',           # autodoc with maths
+    'sphinx.ext.napoleon'           # For auto-doc configuration
+]
+
+napoleon_google_docstring = False   # Turn off googledoc strings
+napoleon_numpy_docstring = True     # Turn on numpydoc strings
+napoleon_use_ivar = True 	     # For maths symbology
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +32,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'Read the Docs' # sphinx_rtd_theme
 html_static_path = ['_static']
+
+
+sys.path.insert(0, os.path.abspath('../../src/bdt/apps'))
+sys.path.insert(0, os.path.abspath('../../src/bdt/utils'))
+sys.path.insert(0, os.path.abspath('.'))

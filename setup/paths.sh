83230db2d66cc8efaa57e1836fe1dc5b206bb36d
imackay <innes.mackay@cern.ch>
2024-10-01 15:58:11 +0100

    Restructure the code

--- setup/paths.sh
+++ setup/paths.sh
@@ -3,4 +3,5 @@ export BASE_DATA_PATH=/data/lhcb/users/mackay/Lambda_c_BDT/
 export DATA_PATH=/data/lhcb/users/mackay/Lambda_c_BDT/data/
 export MC_PATH=/data/lhcb/users/mackay/Lambda_c_BDT/MC/
 
-export PYTHONPATH=${PYTHONPATH}:${BASE}/
+export PYTHONPATH=${PYTHONPATH}:${BASE}/src/bdt/apps
+export PYTHONPATH=${PYTHONPATH}:${BASE}/src/bdt/utils
